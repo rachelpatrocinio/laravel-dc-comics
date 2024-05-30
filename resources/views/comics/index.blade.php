bfad4bdcb7b64127263dbe7892285b1b0a25308a
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-30 16:03:59 +0200

    Refactor: index and create

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 @section('content')
 <h1>Index</h1>
-<ul>
+<p><a href="{{ route('comics.create')}}">Create</a></p>
+<ul class="d-flex flex-wrap list-style-none">
     @foreach($comics as $comic)
-    <li>
-        <h3>{{ $comic->title }}</h3>
-        <p>{{ $comic->description}}</p>
-        <p>{{ $comic->thumb}}</p>
-        <p>{{ $comic->price}}</p>
-        <p>{{ $comic->series}}</p>
-        <p>{{ $comic->sale_date}}</p>
-        <p>{{ $comic->type}}</p>
+    <li class="col-3 p-2">
+        <div class="card p-3 h-100">
+            <p><strong>{{ $comic->title }}</strong></p>
+            <img src="{{$comic->thumb}}">
+            <p>{{ $comic->price}}</p>
+            <p>{{ $comic->series}}</p>
+            <p>{{ $comic->sale_date}}</p>
+            <p>{{ $comic->type}}</p>
+        </div>
     </li>
     @endforeach
 </ul>
