7eaa7c34c6a8f554b22d95181cec52829bc7c43b
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-30 14:25:49 +0200

    Added: datas on index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.app')
 @section('content')
 <h1>Index</h1>
+<ul>
+    @foreach($comics as $comic)
+    <li>
+        <h3>{{ $comic->title }}</h3>
+        <p>{{ $comic->description}}</p>
+        <p>{{ $comic->thumb}}</p>
+        <p>{{ $comic->price}}</p>
+        <p>{{ $comic->series}}</p>
+        <p>{{ $comic->sale_date}}</p>
+        <p>{{ $comic->type}}</p>
+    </li>
+    @endforeach
+</ul>
 @endsection
